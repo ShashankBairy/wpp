28c7f31da3466a712cffb645f0340949d1390e61
*ShashankBairy <*shashankshivakarthik007@gmail.com>
2024-05-29 22:55:22 +0530

    wpp6

---
+++ wpp6.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
+let num =21;
+if( num%3 == 0 && num%7==0){
+    console.log('Multiple of 3 and 7');
+}
+else{
+    console.log('Not Multiple of 3 and 7');
+}
