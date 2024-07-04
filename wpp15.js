836cad5f1f48654f9c372dc8d1447a617cd20962
*ShashankBairy <*shashankshivakarthik007@gmail.com>
2024-07-04 13:01:46 +0530

    wpp15

---
+++ wpp15.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to compute the sum of three elements of a given array of integers of length 3
+
+function wpp15(number){
+    return number[0] + number[1] + number[2];
+}
+
+console.log(wpp15([1, 2, 3]));
